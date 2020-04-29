6f5ec5ff0db93d06436df0ff18ac62025e68be97
hobyanakkathil <hobyanakkathil@gmail.com>
2020-04-29 17:33:04 +0530

    Updates

---
+++ precache-manifest.04bfe69d048eae7d32de8a1f251618b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5a8222c168c2ec52e5d4537ad504f3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ed797d3af85dafd2e7e9",
+    "url": "/my-app/static/css/main.6413fbae.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/my-app/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed797d3af85dafd2e7e9",
+    "url": "/my-app/static/js/main.868e47ca.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
